a8db25150eaaae36fe753e1674525f2c9d6ac017
Abu Hurairah <hurairahmughal17@gmail.com>
2024-07-05 02:24:49 +0500

    Authentication

--- auth_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path("",Home,name='Home'),
-    path('login-page/',Login,name='Login'),
-    path('sign-in/',Sign_in,name='Sign-in'),
-    path('logout/',Logout,name='Logout'),
-]
